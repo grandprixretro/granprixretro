7b20b0fa016ecf9c8b624634d8a8af079ade66ea
grandprixretro <ferhfonta2@gmail.com>
2025-06-21 22:46:37 -0300

    Update saveManager.js

--- saveManager.js
+++ saveManager.js
@@ -6,3 +6,12 @@ function loadGameData() {
   const data = localStorage.getItem("gpmretro_save");
   return data ? JSON.parse(data) : null;
 }
+
+function saveClassificacao(classificacao) {
+  localStorage.setItem("gpmretro_classificacao", JSON.stringify(classificacao));
+}
+
+function loadClassificacao() {
+  const data = localStorage.getItem("gpmretro_classificacao");
+  return data ? JSON.parse(data) : { pilotos: [], equipes: [] };
+}
