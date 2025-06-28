4dd377d660e2a82df35f110cd08dd6528a73828f
grandprixretro <ferhfonta2@gmail.com>
2025-06-28 17:08:30 -0300

    Delete saveManager.js

--- saveManager.js
+++
@@ -1,8 +0,0 @@
-function loadGameData() {
-  const data = localStorage.getItem("savegame");
-  return data ? JSON.parse(data) : null;
-}
-
-function saveGameData(data) {
-  localStorage.setItem("savegame", JSON.stringify(data));
-}
