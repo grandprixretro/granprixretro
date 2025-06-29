a95b0ec86b2e78fe24ad6e131de6f57e1be97952
grandprixretro <ferhfonta2@gmail.com>
2025-06-28 22:16:26 -0300

    Update saveManager.js

--- saveManager.js
+++ saveManager.js
@@ -1,8 +1,9 @@
+// saveManager.js
 export function loadGameData() {
   const data = localStorage.getItem("savegame");
   return data ? JSON.parse(data) : null;
 }
 
-export function saveGame(data) {
+export function saveGameData(data) {
   localStorage.setItem("savegame", JSON.stringify(data));
 }
